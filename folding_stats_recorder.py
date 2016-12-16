2a051790660495f343ea21328f0fa37e2ec44171
Unknown <justin@lulejian.com>
2016-12-15 23:10:47 -0800

    Fixed py docstring, wrong example.

--- folding_stats_recorder.py
+++ folding_stats_recorder.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python
 """An application to record a user's Folding@Home user stats over time.
 
-Warning: This script is only meant to be run hourly, as F@H has explicitly
-  stated that queries of this data should not exceed 24 times in a day (hourly).
+Warning: This script is meant to be run hourly, as F@H has explicitly
+stated that queries of this data should not exceed 24 times in a day (hourly).
 
 Example usage:
-  folding_stats_recorder --username='PS3EdOlkkola' --record_location=/home/john
-    --userdata_location='http://fah-web.stanford.edu/daily_user_summary.txt.bz2'
+  python folding_stats_recorder.py --username='PS3EdOlkkola' 
+    --record_location=/home/$USER/fahuserdata.csv
 
-Note: On successive runs script will append data if the same record_location is
-  specified.
+Defaults: 'record_location' defaults to the above. 'username' defaults to 
+'anonymous'.
+
+Note: On successive runs script will append data to the csv if the same 
+record_location is specified.
 """
 # pylint: disable=fixme
 # TODO(justinlulejian): unit tests.
